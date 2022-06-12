7cddeea666d8416bc6a92b5944d46cc8bd41a296
kunnoh <alvindereba@gmail.com>
2022-06-13 00:55:54 +0300

    built frontend

---
+++ pern-ui/build/precache-manifest.0277519bf06e3d13b4919fe35a0dbf11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49b7a85a1ce9289780b",
+    "url": "/static/css/main.280f3f9d.chunk.css"
+  },
+  {
+    "revision": "c49b7a85a1ce9289780b",
+    "url": "/static/js/main.c49b7a85.chunk.js"
+  },
+  {
+    "revision": "30e9960dcfd0682496ad",
+    "url": "/static/js/1.30e9960d.chunk.js"
+  },
+  {
+    "revision": "7559484b6d66a5127296",
+    "url": "/static/js/2.7559484b.chunk.js"
+  },
+  {
+    "revision": "38d81f54dccc8e7b761d",
+    "url": "/static/js/runtime~main.38d81f54.js"
+  },
+  {
+    "revision": "6bcd7ce4d44c71dcb80e9edb0159fbf0",
+    "url": "/index.html"
+  }
+];
