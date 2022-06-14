8854bef2ac45ffabd0f564e7851e913337e202ad
kunnoh <alvindereba@gmail.com>
2022-06-14 11:33:57 +0300

    images running fine

--- sql/init.sql
+++ sql/init.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE schmanage;
 GRANT ALL PRIVILEGES ON DATABASE schmanage TO postgres;
+
+CREATE TABLE users (
+  user_id serial PRIMARY KEY,
+  email varchar (100) NOT NULL,
+  firstname varchar (100) NOT NULL,
+  lastname varchar (100) NOT NULL,
+  created_at date
+);
